27f4522e7f1e00e931ce4a8f2d9410d634cf6275
Hafiz Hanif <apitlekays@gmail.com>
2016-10-17 20:38:48 +0800

    corrected view for additional pages

--- page.php
+++ page.php
@@ -13,9 +13,7 @@
 			    <?php endwhile; endif; ?>							
 			    					
 			</main> <!-- end #main -->
-
-		    <?php get_sidebar(); ?>
-		    
+  
 		</div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->

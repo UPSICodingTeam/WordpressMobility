6cfb7339d9e98bbad784584970a572d1b83bf9ca
Hafiz Hanif <apitlekays@gmail.com>
2016-10-17 20:38:32 +0800

    made page receive dynamic data

--- page-programmes.php
+++ page-programmes.php
@@ -1,110 +1,39 @@
 <?php get_header(); ?>
 	
-	<div class="row">
-				<div class=" medium-6 small-12 columns">
-					<h4>Faculty of Languages and Communication</h4>
-					<ul>
-						<li>Bachelor of Education (Malay Language) with Honours </li>
-						<li>Bachelor of Education (Teaching English as a Second Language -TESL) with Honours </li>
-						<li>Bachelor of Education (Malay Literature) with Honours </li>
-						<li>Bachelor in Arabic Language with Education</li>
-						<li>Bachelor in Chinese Language with Education</li>
-						<li>Bachelor in Education (Tamil Language) with Honours </li>
-						<li>Bachelor in Communication (Social Communication) with Honours </li>
-						<li>Diploma in English </li>
+			<div class="row">
+				<div class=" medium-8 medium-offset-2 small-12 columns">
+					<ul class="accordion" data-accordion>
+					<?php
+					$posts = get_posts(array(
+						'post_type'			=> 'programmes_offered',
+					));
+						if( $posts ):
+							foreach( $posts as $post ): 
+								setup_postdata( $post );
+								// Variables for Staffs Page
+								$facultyName	= get_field('facultys_name');
+								$programmeName	= get_field('programmes_offered');
+								?>	
+					
+					  <li class="accordion-item" data-accordion-item>
+					    <a href="#" class="accordion-title"><h5><?php echo $facultyName; ?></h5></a>
+					    <div class="accordion-content" data-tab-content>
+					     <ul>
+						     <?php if(have_rows('programmes_offered')):
+							     	while(have_rows( 'programmes_offered')):
+								     	the_row(); ?>
+								     		<li><a href="<?php the_sub_field('programme_url');?>"><?php the_sub_field('programme_title');?></a></li>
+								     	<?php
+							     	endwhile; 
+						     endif; ?>
+						</ul>
+					    </div>
+					  </li>
+												
+						<?php endforeach; ?>
 					</ul>
-					
-					<h4>Faculty of Art Computing and Creative Industry </h4>
-					<ul>
-						<li>Bachelor of Education (Information Technology) with Honours </li>
-						<li>Bachelor of Education (Multimedia) with Honours </li>
-						<li>Bachelor of Education (Computer Aided Design Technology) with Honours </li>
-						<li>Bachelor of Design (Animation) with Honours </li>
-						<li>Bachelor of Education (Art) with Honours </li>
-						<li>Bachelor of Education (Visual Art Communication) with Honours </li>
-						<li>Bachelor of Design (Advertising) with Honours </li>
-						<li>Bachelor of Software Engineering (Education Software) with Honours </li>
-						<li>Diploma in Design and Games Development</li>
-						<li>Diploma in Computer Science (Internet Computing)</li>
-					</ul>
-					
-					
-					<h4>Faculty of Science and Mathematics </h4>
-					<ul>
-						<li>Bachelor of Education (Science) with Honours </li>
-						<li>Bachelor of Education (Mathematics) with Honours </li>
-						<li>Bachelor of Science (Mathematics)  with Education</li>
-						<li>Bachelor of Education (Biology) with Honours </li>
-						<li>Bachelor of Education (Chemistry) with Honours </li>
-						<li>Bachelor of Education (Physics) with Honours </li>
-						<li>Diploma in Science</li>
-						<li>Diploma in Science (Mathematics)</li>
-						<li>Diploma in Laboratory Technology</li>
-					</ul>
-					
-					
-					
-					<h4>Faculty of  Education and Human Development </h4>
-					<ul>
-						<li>Bachelor of Education (Guidance & Counseling) with Honours </li>
-						<li>Bachelor of Education (Early Childhood) with Honours </li>
-						<li>Bachelor of Education (Special Education) with Honours </li>
-						<li>Bachelor of Psychology with Honours </li>
-						<li>Diploma in Early Childhood Education</li>
-					</ul>
-					
-				</div>
-				<div class="medium-6 small-12 columns">
-					<h4>Faculty of Management and Economics </h4>
-					<ul>
-						<li>Bachelor of Education (Accountancy) with Honours </li>
-						<li>Bachelor of Education (Business Management) with Honours </li>
-						<li>Bachelor of Education (Economics) with Honours </li>
-						<li>Bachelor of Education (Educational Management) with Honours </li>
-						<li>Bachelor of Education (Entrepreneurship & Commerce) with Honours </li>
-						<li>Bachelor of Accountancy with Honours </li>
-						<li>Bachelor of Economics (Islamic Finance) with Honours </li>
-						<li>Bachelor of Economics (Finance) with Honours </li>
-						<li>Bachelor of Business Administration (Human Resource Management) with Honours </li>
-						<li>Bachelor of Business Administration (Finance Services) with Honours </li>
-						<li>Diploma in Entrepreneurship</li>
-					</ul>
-					
-					
-					<h4>Faculty of Sports Science and Coaching </h4>
-					<ul>
-						<li>Bachelor of Education (Sport Science) with Honours </li>
-						<li>Bachelor of Education (Physical Education) with Honours </li>
-						<li>Bachelor of Sport Science (Sports Rehabilitation) with Honours </li>
-						<li>Bachelor of Sport Science (Sports Psychology) with Honours </li>
-						<li>Bachelor of Sport Science (Coaching Science) with Honours</li>
-					</ul>
-					 
-					
-					<h4>Faculty of Technical and Vocational Education </h4>
-					<ul>
-						<li>Bachelor of Education (Living Skills) with Honours </li>
-						<li>Bachelor of Education (Home Economics) with Honours </li>
-						<li>Bachelor of Education (Agricultural Science) with Honours </li>
-					</ul>
-					
-					
-					<h4>Faculty of Human Sciences </h4>
-					<ul>
-						<li>Bachelor of Education (History) with Honours </li>
-						<li>Bachelor of Education (Geography) with Honours </li>
-						<li>Bachelor of Education (Moral Education) with Honours </li> 
-						<li>Bachelor of Education (Islamic Education) with Honours </li>
-						<li>Bachelor of Malaysian Studies with Education with Honours </li>
-					</ul>
-					
-					<h4>Faculty of Music and Performing Arts</h4>
-					<ul>
-						<li>Bachelor of Education (Music)</li>
-						<li>Bachelor of Performing Arts (Dance)</li>
-						<li>Bachelor of Performing Arts (Theatre)</li>
-						<li>Diploma of Music</li>
-					</ul>
+						<?php wp_reset_postdata(); ?>
+						<?php endif; ?>					
 				</div>
 			</div>
 		</section>

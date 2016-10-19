e4e7439657c5fcb230835e427d2a6ceae0fda2f3
Hafiz Hanif <apitlekays@gmail.com>
2016-10-19 11:55:26 +0800

    added the custom dashboard codes

--- assets/functions/admin.php
+++ assets/functions/admin.php
@@ -25,62 +25,21 @@ For more information on creating Dashboard Widgets, view:
 http://digwp.com/2010/10/customize-wordpress-dashboard/
 */
 
-// RSS Dashboard Widget
-function joints_rss_dashboard_widget() {
-	if(function_exists('fetch_feed')) {
-		include_once(ABSPATH . WPINC . '/feed.php');               // include the required file
-		$feed = fetch_feed('http://jointswp.com/feed/rss/');        // specify the source feed
-		$limit = $feed->get_item_quantity(5);                      // specify number of items
-		$items = $feed->get_items(0, $limit);                      // create an array of items
-	}
-	if ($limit == 0) echo '<div>' . __( 'The RSS Feed is either empty or unavailable.', 'jointswp' ) . '</div>';   // fallback message
-	else foreach ($items as $item) { ?>
-
-	<h4 style="margin-bottom: 0;">
-		<a href="<?php echo $item->get_permalink(); ?>" title="<?php echo mysql2date(__('j F Y @ g:i a', 'jointswp'), $item->get_date('Y-m-d H:i:s')); ?>" target="_blank">
-			<?php echo $item->get_title(); ?>
-		</a>
-	</h4>
-	<p style="margin-top: 0.5em;">
-		<?php echo substr($item->get_description(), 0, 200); ?>
-	</p>
-	<?php }
-}
-
-//Just testing Dashboard Widget
-function hz_test_dashboard_widget() {
-	
-	?>
-	<div id="dashboard-widgets-wrap">
-	    <div id="dashboard-widgets" class="metabox-holder">
-	        <div class="postbox-container" width="%">
-	            <div id="normal-sortables" class="ui-sortable meta-box-sortable">
-	                <!-- BOXES -->
-	                <div class="postbox">...</div>
-	            </div>
-	        </div>
-	        <div class="postbox-container" width="%">...</div>
-	        <div class="postbox-container" width="%">...</div>
-	        <div class="postbox-container" width="%">...</div>
-	    </div>
-	</div>
-	<?php
-}
 
 // Calling all custom dashboard widgets
-function joints_custom_dashboard_widgets() {
-	wp_add_dashboard_widget('jhz_test_dashboard_widget', __('Mobility Unit Statistics', 'jointswp'), 'hz_test_dashboard_widget');
-	wp_add_dashboard_widget('joints_rss_dashboard_widget', __('Custom RSS Feed (Customize in admin.php)', 'jointswp'), 'joints_rss_dashboard_widget');
+//function joints_custom_dashboard_widgets() {
+		//wp_add_dashboard_widget('joints_rss_dashboard_widget', __('Custom RSS Feed (Customize in admin.php)', 'jointswp'), 'joints_rss_dashboard_widget');
 	
 	/*
 	Be sure to drop any other created Dashboard Widgets
 	in this function and they will all load.
 	*/
-}
+//}
+
 // removing the dashboard widgets
 add_action('admin_menu', 'disable_default_dashboard_widgets');
 // adding any custom widgets
-add_action('wp_dashboard_setup', 'joints_custom_dashboard_widgets');
+//add_action('wp_dashboard_setup', 'joints_custom_dashboard_widgets');
 
 /************* CUSTOMIZE ADMIN *******************/
 // Custom Backend Footer
@@ -90,3 +49,96 @@ function joints_custom_admin_footer() {
 
 // adding it to the admin area
 add_filter('admin_footer_text', 'joints_custom_admin_footer');
+
+/////.....................................////
+
+// Dashboard Widget for # of Published Courses and Programmes
+function custom_dashboard_widget() {
+	
+	$summer_count  		= wp_count_posts('summer_camp');
+	$number_sum			= $summer_count->publish;
+	$exchange_count		= wp_count_posts('exchange_programmes');
+	$number_ex			= $exchange_count->publish;
+	
+	echo "
+	<div class='row text-center'>
+		<div class='small-6 columns'>
+			<p style='font-size:10em; font-weight:bold; margin: 0px; padding-left:50px;'>",$number_sum,"</p>
+		</div>
+		<div class='small-6 columns'>
+			<p style='font-size:10em; font-weight:bold; margin: 0px; padding-left:50px;'>",$number_ex,"</p>
+		</div>
+	</div>
+	<div class='row text-center'>
+		<div class='small-6 columns'>
+			<p style='font-size:1em; margin: 0px; padding-left:25px;'>Summer Camps</p>
+		</div>
+		<div class='small-6 columns'>
+			<p style='font-size:1em; margin: 0px; padding-left:25px;'>Exchange Programmes</p>
+		</div>
+	</div>";
+}
+function add_custom_dashboard_widget() {
+	wp_add_dashboard_widget('custom_dashboard_widget', 'Number of Published Courses and Programmes', 'custom_dashboard_widget');
+}
+add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
+
+// Dashboard Widget for # of Courses & Programmes Under Review
+function custom_dashboard_widget1() {
+	$summer_count  		= wp_count_posts('summer_camp');
+	$number_sum			= $summer_count->draft;
+	$exchange_count		= wp_count_posts('exchange_programmes');
+	$number_ex			= $exchange_count->draft;
+	
+	echo "
+	<div class='row text-center'>
+		<div class='small-6 columns'>
+			<p style='font-size:10em; font-weight:bold; margin: 0px; padding-left:50px;'>",$number_sum,"</p>
+		</div>
+		<div class='small-6 columns'>
+			<p style='font-size:10em; font-weight:bold; margin: 0px; padding-left:50px;'>",$number_ex,"</p>
+		</div>
+	</div>
+	<div class='row text-center'>
+		<div class='small-6 columns'>
+			<p style='font-size:1em; margin: 0px; padding-left:25px;'>Summer Camps</p>
+		</div>
+		<div class='small-6 columns'>
+			<p style='font-size:1em; margin: 0px; padding-left:25px;'>Exchange Programmes</p>
+		</div>
+	</div>";
+}
+function add_custom_dashboard_widget1() {
+	wp_add_dashboard_widget('custom_dashboard_widget1', 'Number of Courses and Programmes Under Review', 'custom_dashboard_widget1');
+}
+add_action('wp_dashboard_setup', 'add_custom_dashboard_widget1');
+
+// Dashboard Widget for # of Applications
+function custom_dashboard_widget2() {
+	$application_count  	= wp_count_posts('application');
+	$number_app			   	= $application_count->publish;
+
+	
+	echo "
+	<div class='row text-center'>
+		<div class='small-6 columns'>
+			<p style='font-size:10em; font-weight:bold; margin: 0px; padding-left:50px;'>",$number_app,"</p>
+		</div>
+		<div class='small-6 columns'>
+			<p style='font-size:10em; font-weight:bold; margin: 0px; padding-left:50px;'>",$number_ex,"</p>
+		</div>
+	</div>
+	<div class='row text-center'>
+		<div class='small-6 columns'>
+			<p style='font-size:1em; margin: 0px; padding-left:25px;'>Submitted</p>
+		</div>
+		<div class='small-6 columns'>
+			<p style='font-size:1em; margin: 0px; padding-left:25px;'>Processed</p>
+		</div>
+	</div>";
+}
+function add_custom_dashboard_widget2() {
+	wp_add_dashboard_widget('custom_dashboard_widget2', 'Number of Student Applications', 'custom_dashboard_widget2');
+}
+add_action('wp_dashboard_setup', 'add_custom_dashboard_widget2');
+
